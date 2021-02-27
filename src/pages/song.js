93d0134112bee24b5bbb9098d769bb7dae11521b
TwinkleDing <32945061+TwinkleDing@users.noreply.github.com>
2021-02-27 10:37:55 +0800

    增加自定义操作“

--- src/pages/song.js
+++ src/pages/song.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import '../css/song.scss';
-
+import songList from '../songList/list';
 export default class Song extends React.Component{
   render() {
     return <div className='song'>
-      <audio controls></audio>
+      <div>{songList[0].name}</div>
+      <Control></Control>
+    </div>
+  }
+}
+
+class Control extends React.Component{
+  constructor(props) {
+    super(props);
+    this.myRef = React.createRef();
+  }
+  start = ()=> {
+    this.myRef.current.play()
+  }
+  render() {
+    return <div>
+      <audio ref={this.myRef} src={songList[0].url} controls></audio>
+      <div onClick={this.start}>开始</div>
     </div>
   }
 }
---
+++ src/songList/list.js
@@ -0,0 +1,8 @@
+let list = [
+  {
+    name: 'ice.m4a',
+    url: 'https://ws.stream.qqmusic.qq.com/C400003AveiP0xwLPJ.m4a?guid=3177725952&vkey=68B2C0A9C39CC8B5AB31C0E0F619A89799BC1D9F846B2583EE3B0CEE8F008F54E873830E82C6C9DC3914C8F4FB949484C87C71C370DB6B33&uin=0&fromtag=66'
+  }
+]
+
+export default list
